2be79b0499f2ae0d68249eb3a7bcf17dd0885f89
Amanda Albuquerque <amandlei@hotmail.com>
2020-03-31 15:28:52 -0300

    add Final container

---
+++ src/containers/Final/Final.spec.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Final from './Final'
+
+describe('Final > ', () => {
+  it('renders without crashing', () => {
+    shallow(<Final />)
+  })
+})
