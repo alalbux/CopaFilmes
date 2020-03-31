79562b8f80554eea7f467cea400dc619b7d1268f
Amanda Albuquerque <amandlei@hotmail.com>
2020-03-31 16:36:15 -0300

    add components index

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+// cards
+export { default as Card } from './cards/Card/Card'
+
+// content
+export { default as Button } from './content/Button/Button'
+export { default as Text } from './content/Text/Text'
+export { default as Title } from './content/Title/Title'
+
+// layout
+export { default as Flexbox } from './layout/Flexbox/Flexbox'
+export { default as Header } from './layout/Header/Header'
+export { default as Page } from './layout/Page/Page'
