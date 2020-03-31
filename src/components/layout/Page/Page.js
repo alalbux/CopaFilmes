9d6d2c58a8cf8d45641b64a442f7110ea7fd1e5d
Amanda Albuquerque <amanda.albuquerque@laborit.com.br>
2020-03-31 16:35:42 -0300

    add Page basic component

---
+++ src/components/layout/Page/Page.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { oneOfType, node, func } from 'prop-types'
+import styled from 'styled-components'
+
+const PageContainer = styled.div`
+  background-color: #a8a8a8;
+  max-width: 1360px;
+  padding: 36px;
+  width: 100%;
+  margin: 0 auto;
+  height: 100%;
+`
+
+const Page = ({ children }) => (
+  <PageContainer>
+    {children}
+  </PageContainer>
+)
+
+Page.propTypes = {
+  children: oneOfType([node, func])
+}
+
+Page.displayName = 'Page'
+
+export default Page
