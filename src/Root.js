79bf385f2ec42ea50c7f06d4a9d87842a5f9c898
Amanda Albuquerque <amandlei@hotmail.com>
2020-03-31 13:25:22 -0300

    add style and basic structure

---
+++ src/Root.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './containers/Home'
+import GlobalStyles from './styles/globalStyles'
+
+const Root = () => (
+  <main>
+    <GlobalStyles />
+    <Home />
+  </main>
+)
+
+export default Root
