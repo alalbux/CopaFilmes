bbfbd9436d0f1888117d96519a983829c0cb4cfb
Amanda Albuquerque <amanda.albuquerque@laborit.com.br>
2020-04-02 10:44:03 -0300

    add [GET] Movies list

---
+++ api/Models/Movie.cs
@@ -0,0 +1,8 @@
+namespace FilmsChampions.Models {
+    public class Movie {
+        public string Id { get; set; }
+        public string Titulo { get; set; }
+        public int Ano { get; set; }
+        public double Nota { get; set; }
+    }
+}
